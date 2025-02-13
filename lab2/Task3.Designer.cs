ab588f5cb64528fb369846fea138c1b4d44c383b
GreyAsher <mopadaadrian@gmail.com>
2025-02-13 13:17:24 +0800

    Lab2(Changes)

---
+++ lab2/Task3.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace lab2
+{
+    partial class Lab2Task3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtInput = new TextBox();
+            btnCalculate = new Button();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(76, 54);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(125, 27);
+            txtInput.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(86, 133);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(94, 29);
+            btnCalculate.TabIndex = 1;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(101, 212);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(57, 20);
+            lblResult.TabIndex = 2;
+            lblResult.Text = "Answer";
+            // 
+            // Lab2Task3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(340, 450);
+            Controls.Add(lblResult);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtInput);
+            Name = "Lab2Task3";
+            Text = "Task3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtInput;
+        private Button btnCalculate;
+        private Label lblResult;
+    }
+}
---
+++ lab2/Task3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Lab2Task3 : Form
+    {
+        public Lab2Task3()
+        {
+            InitializeComponent();
+        }
+
+        private int Fibonacci(int n)
+        {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtInput.Text, out int n) && n >= 0)
+            {
+                int result = Fibonacci(n);
+                lblResult.Text = $"Fibonacci({n}) = {result}";
+            }
+            else
+            {
+                lblResult.Text = "Please enter a valid non-negative number.";
+            }
+        }
+    }
+}
